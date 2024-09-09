ca2f8033994ec3a0a8ab605c3a6982ef07c91a5d
Sankalp-1711 <sankalpgoyal1711@gmail.com>
2024-09-09 23:37:53 +0530

    changes done

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Content.scss';
 import { useImageSize } from 'react-image-size';
+import Frame1 from '../../assets/Frame43.png';
+import Frame2 from '../../assets/Frame44.png';
+import Frame3 from '../../assets/Frame45.png';
+import Frame4 from '../../assets/Frame46.png';
+import Hero from '../../assets/Hero.png'
+
 const Content = () => {
 
   async function fetchImageSize() {
     try {
-        const dimensions = await getImageSize('Hero.png');
+        const dimensions = await getImageSize({Hero});
         console.log(dimensions);
     } catch (error) {
         console.error(error);
@@ -44,10 +50,10 @@ const Content = () => {
       <section className="follow-us-section">
         <h3>Follow Us</h3>
         <div className="gallery">
-          <img src="Frame 43.png" alt="Gallery 1" />
-          <img src="Frame 44.png" alt="Gallery 2" />
-          <img src="Frame 45.png" alt="Gallery 3" />
-          <img src="Frame 46.png" alt="Gallery 4" />
+          <img src={Frame1} alt="Gallery 1" />
+          <img src={Frame2} alt="Gallery 2" />
+          <img src={Frame3} alt="Gallery 3" />
+          <img src={Frame4} alt="Gallery 4" />
         </div>
       </section>
     </div>
